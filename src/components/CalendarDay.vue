927792e6278d9a4465f188d23d73a19b2ad54fd7
Rachel Gould <rachel@rachelgould.dev>
2020-11-23 14:54:48 -0800

    form submissions add to credit and debit arrays

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,14 +1,40 @@
 <template>
   <div v-bind:class="[editMode ? 'day-editMode' : 'day']">
     {{ day }} || 
-    <span v-if="credits.length">
+    <span v-if="credits.length && !editMode">
       +{{ credits }}{{' '}}
     </span>
-    <span v-if="debits.length">
+    <span v-if="debits.length && !editMode">
       -{{ debits }}{{' '}}
     </span>
+    <div v-if="editMode">
+      Credits:
+      <ul>
+        <li v-for="(credit, i) in credits" :key="`c-${day}-${i}`">
+          {{credit}}
+          <button>x</button>
+        </li>
+      </ul>
+      <form @submit.prevent="onSubmitCredit">
+        <input v-model.number="newCredit">
+        <input class="button" type="submit" value="Submit"> 
+      </form>
+    </div>
+    <div v-if="editMode">
+      Debits:
+      <ul>
+        <li v-for="(debit, i) in debits" :key="`d-${day}-${i}`">
+          {{debit}}
+          <button>x</button>
+        </li>
+      </ul>
+      <form @submit.prevent="onSubmitDebit">
+        <input v-model.number="newDebit">
+        <input class="button" type="submit" value="Submit"> 
+      </form>
+    </div>
     <button v-on:click="toggleEditMode">
-      {{!editMode ? 'Edit' : 'Save'}}
+      {{!editMode ? 'Edit' : 'Close'}}
     </button>
   </div>
 </template>
@@ -26,13 +52,23 @@ export default {
     return {
       debits: [],
       credits: [],
-      editMode: false
+      editMode: false,
+      newDebit: null,
+      newCredit: null
     }
   },
   methods: {
     toggleEditMode() {
       let old = this.editMode;
       this.editMode = !old;
+    },
+    onSubmitCredit() {
+      this.credits.push(this.newCredit);
+      this.newCredit = null;
+    },
+    onSubmitDebit() {
+      this.debits.push(this.newDebit);
+      this.newDebit = null;
     }
   }
 };
