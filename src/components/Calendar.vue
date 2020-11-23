32d2a30c55a1e12eb3da22b8ec8471a3e47ff5ef
Rachel Gould <rachel@rachelgould.dev>
2020-11-23 14:08:06 -0800

    put calendar days into own component

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
     <p>{{ currentMonth + ' ' + currentDayofMonth + ' ' + currentYear}}</p>
-    <ul>
-      <li v-for="day in daysInMonth" :key="day">
-        {{day}}
-      </li>
-    </ul>
+    <div>
+      <CalendarDay 
+        v-for="day in daysInMonth" 
+        :key="day" 
+        :day="day"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import moment from 'moment';
+import CalendarDay from './CalendarDay';
 
 export default {
+  components: {
+    CalendarDay
+  },
   methods: {
     currentDateTime() {
       return moment().format('MMMM Do YYYY, h:mm:ss a')
---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{ day }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    day: {
+      type: Number,
+      required: true
+    }
+  },
+};
+</script>
