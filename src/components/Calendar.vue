c776518d3d1351b645ea524d088d682b9c0a9b4d
Rachel Gould <rachel@rachelgould.dev>
2020-11-23 13:45:38 -0800

    use moment to compute datetime

---
+++ src/components/Calendar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{currentDateTime()}}</p>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  methods: {
+    currentDateTime() {
+      return moment().format('MMMM Do YYYY, h:mm:ss a')
+    }
+  }
+};
+</script>
