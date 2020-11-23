cad8f256591526f5484db7ec6f08347db460a9b9
Rachel Gould <rachel@rachelgould.dev>
2020-11-23 14:20:56 -0800

    store and pass debits and credits

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -6,6 +6,8 @@
         v-for="day in daysInMonth" 
         :key="day" 
         :day="day"
+        :debits="entries[day]['debits']"
+        :credits="entries[day]['credits']"
       />
     </div>
   </div>
@@ -18,6 +20,136 @@ import CalendarDay from './CalendarDay';
 export default {
   components: {
     CalendarDay
+  },
+  data() {
+    return {
+      entries: {
+        1: {
+          debits: [],
+          credits: []
+        },
+        2: {
+          debits: [],
+          credits: []
+        },
+        3: {
+          debits: [],
+          credits: []
+        },
+        4: {
+          debits: [],
+          credits: []
+        },
+        5: {
+          debits: [],
+          credits: []
+        },
+        6: {
+          debits: [],
+          credits: []
+        },
+        7: {
+          debits: [],
+          credits: []
+        },
+        8: {
+          debits: [],
+          credits: []
+        },
+        9: {
+          debits: [],
+          credits: []
+        },
+        10: {
+          debits: [],
+          credits: []
+        },
+        11: {
+          debits: [],
+          credits: []
+        },
+        12: {
+          debits: [],
+          credits: []
+        },
+        13: {
+          debits: [],
+          credits: []
+        },
+        14: {
+          debits: [],
+          credits: []
+        },
+        15: {
+          debits: [],
+          credits: []
+        },
+        16: {
+          debits: [],
+          credits: []
+        },
+        17: {
+          debits: [],
+          credits: []
+        },
+        18: {
+          debits: [],
+          credits: []
+        },
+        19: {
+          debits: [],
+          credits: []
+        },
+        20: {
+          debits: [],
+          credits: []
+        },
+        21: {
+          debits: [],
+          credits: []
+        },
+        22: {
+          debits: [],
+          credits: []
+        },
+        23: {
+          debits: [],
+          credits: []
+        },
+        24: {
+          debits: [],
+          credits: []
+        },
+        25: {
+          debits: [],
+          credits: []
+        },
+        26: {
+          debits: [],
+          credits: []
+        },
+        27: {
+          debits: [],
+          credits: []
+        },
+        28: {
+          debits: [],
+          credits: []
+        },
+        29: {
+          debits: [],
+          credits: []
+        },
+        30: {
+          debits: [],
+          credits: []
+        },
+        31: {
+          debits: [],
+          credits: []
+        }
+      }
+    }
   },
   methods: {
     currentDateTime() {
