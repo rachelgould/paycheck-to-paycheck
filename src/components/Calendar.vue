7b2e5898709b5706198b941ed9925d21f1a9d326
Rachel Gould <rachel@rachelgould.dev>
2020-11-23 13:57:42 -0800

    list days in month

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
-    <p>{{currentDateTime()}}</p>
+    <p>{{ currentMonth + ' ' + currentDayofMonth + ' ' + currentYear}}</p>
+    <ul>
+      <li v-for="day in daysInMonth" :key="day">
+        {{day}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import moment from 'moment'
+import moment from 'moment';
 
 export default {
   methods: {
     currentDateTime() {
       return moment().format('MMMM Do YYYY, h:mm:ss a')
+    }
+  },
+  computed: {
+    currentMonth() {
+      return moment().format('MMMM');
+    },
+    currentYear() {
+      return moment().format('YYYY');
+    },
+    currentDayofMonth() {
+      return moment().format('DD');
+    },
+    daysInMonth() {
+      return moment().daysInMonth();
     }
   }
 };
