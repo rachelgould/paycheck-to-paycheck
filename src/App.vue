ad9336864b07f555c3dc0ed39e993eefe1610bea
Rachel Gould <rachel@rachelgould.dev>
2020-11-23 18:59:17 -0800

    bold and new line instructions

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
       <h2 class="darkgreen font-medium text-xl text-center mb-4">
         Wondering how much money you'll have at the end of the month?
       </h2>
-      <p class="darkgreen"><span class="font-medium">Tip:</span> Start by entering how much is in your account right now, and then fill in any payments or expenses for the remainder of the month. Adding credits/debits for days that have already passed will not affect your total, since they should be reflected in your current account total.</p>
+      <p class="darkgreen"><span class="font-medium">Tip:</span> Start by entering how much is in your account right now, and then fill in any payments or expenses for the remainder of the month.</p>
+      <p class="font-medium">Adding credits/debits for days that have already passed will not affect your total, since they should be reflected in your current account total.</p>
     </div>
     <Calendar />
   </div>
