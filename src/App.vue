3519deadef03dca59c7573fcf8dfa9e9bb8507dd
Rachel Gould <rachel@rachelgould.dev>
2020-11-23 18:11:32 -0800

    add disclaimer, wider margins

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container mx-auto mt-12 px-8">
     <img src="@/assets/images/Paycheck-logo.png" alt="simple budgeting" class="mx-auto mb-8">
-    <div class="text-center mb-8">
+    <div class="text-center mb-8 w-11/12 lg:w-2/3 xl:w-2/3 mx-auto">
       <h2 class="darkgreen font-medium text-xl text-center mb-4">
         Wondering how much money you'll have at the end of the month?
       </h2>
-      <p class="darkgreen"><span class="font-medium">Tip:</span> Start by entering how much is in your account right now, and then fill in any payments or expenses for the remainder of the month.</p>
+      <p class="darkgreen"><span class="font-medium">Tip:</span> Start by entering how much is in your account right now, and then fill in any payments or expenses for the remainder of the month. Adding credits/debits for days that have already passed will not affect your total, since they should be reflected in your current account total.</p>
     </div>
     <Calendar />
   </div>
