f9d02123dc5e703087b548e9dbf1799a4e61dbba
Rachel Gould <rachel@rachelgould.dev>
2020-11-23 16:31:03 -0800

    accept isToday, style when not in edit mode

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,11 +1,22 @@
 <template>
-  <div v-bind:class="[editMode ? 'day-editMode' : 'day']">
-    {{ day }} || 
-    <span v-if="!editMode">
-      +{{ creditTotal }}{{' '}}
+  <div v-bind:class="[editMode ? 'day editMode' : 'day']">
+    <div v-bind:class="[isToday ? 'day-badge today' : 'day-badge']">
+      {{ day }}
+    </div>
+    <span
+      v-if="!editMode && !(creditTotal > 0 || debitTotal > 0)"
+      class="total-nocontent">
+      To add a payment, click "edit"
     </span>
-    <span v-if="!editMode">
-      -{{ debitTotal }}{{' '}}
+    <div 
+      v-if="!editMode && (creditTotal > 0 || debitTotal > 0)" 
+      class="total-content positive">
+       <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path></svg> {{ creditTotal }}
+    </div>
+    <span 
+      v-if="!editMode && (creditTotal > 0 || debitTotal > 0)" 
+      class="total-content negative">
+      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg> {{ debitTotal }}
     </span>
     <div v-if="editMode">
       Credits:
@@ -45,6 +56,10 @@ export default {
   props: {
     day: {
       type: Number,
+      required: true
+    },
+    isToday: {
+      type: Boolean,
       required: true
     }
   },
@@ -91,3 +106,29 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+  .day {
+    @apply flex items-center justify-between rounded-full py-3 px-4 my-4;
+    background-color: #E7EEE8;
+  }
+  .day-badge {
+    @apply rounded-full h-12 w-12 flex items-center justify-center bg-white text-2xl font-bold;
+    color: #83058A;
+  }
+  .day-badge.today {
+    color: #54785A !important;
+  }
+  .total-content {
+    @apply text-lg font-medium flex items-center justify-center;
+  }
+  .total-nocontent {
+    @apply text-sm italic
+  }
+  .positive {
+    @apply text-green-700;
+  }
+  .negative {
+    @apply text-red-700;
+  }
+</style>
